ba2c052a1d751179291510a9eb5e0ac4d9f7e7fb
Gregory Tereshko <gtrsh@users.noreply.github.com>
2025-03-20 17:23:58 +0300

    complete yt-dlp module

--- yt-obs-server/src/yt_obs_server/yt_dlp.clj
+++ yt-obs-server/src/yt_obs_server/yt_dlp.clj
@@ -1,9 +1,31 @@
 (ns yt-obs-server.yt-dlp
   (:require [clojure.java.shell :refer [sh]]))
 
+(def YT-DLP "yt-dlp")
+(def JSON "--dump-single-json")
+
+(def URL-VID "https://www.youtube.com/watch?v=")
+(def URL-CHAN "https://www.youtube.com/")
+
 (defn yt-dlp-version
   []
-  (sh "yt-dlp" "--version" ))
+  (sh YT-DLP "--version"))
+
+(defn yt-dlp-video-data
+  [id]
+  (sh YT-DLP JSON (str URL-VID id)))
+
+(defn yt-dlp-channel-data
+  [channel-id resource-type]
+  (sh YT-DLP JSON (str URL-CHAN channel-id "/" resource-type)))
 
 (comment
-  (yt-dlp-version))
+  (require '[cheshire.core :refer [parse-string]])
+  (yt-dlp-version)
+  (yt-dlp-video-data "UWh1pe3ApDo")
+  (yt-dlp-channel-data "@PiterPyConference" "shorts")
+
+  (parse-string (:out (yt-dlp-video-data "UWh1pe3ApDo")))
+  (parse-string (:out (yt-dlp-channel-data "@PiterPyConference" "shorts")))
+
+  :end)
